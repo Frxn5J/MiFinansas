f74b6202f4df159971cd383717e35b69951edc15
Francisco Castillo <48078177+Frxn5J@users.noreply.github.com>
2025-05-05 20:30:14 -0600

    SplashScreen Agregada

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'screens/profile_screen.dart';
 import 'widgets/main_navigator.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +21,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +29,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'TiltNeon',
       ),
-      home: LoginScreen(),
+      home: FlutterSplashScreen(
+        duration: const Duration(seconds: 3),
+        backgroundColor: Colors.white,
+        nextScreen: LoginScreen(),
+        splashScreenBody: Center(
+          child: Image.asset(
+            'assets/logo.png',
+            width: 256,
+            height: 256,
+          ),
+        ),
+      ),
     );
   }
 }
